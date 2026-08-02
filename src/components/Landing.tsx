3f42dd1713bdb8c419808c427d4ed5502a6a57b1
Tushar Singla <tusharsingla@Tushars-MacBook-Air.local>
2026-08-02 17:28:50 +0530

    feat: display name on one line

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -50,9 +50,7 @@ const Landing = () => {
           <div className="landing-intro">
             <h2>Hello! I'm</h2>
             <h1>
-              TUSHAR
-              <br />
-              <span>SINGLA</span>
+              TUSHAR <span>SINGLA</span>
             </h1>
           </div>
           <div className="landing-info">
